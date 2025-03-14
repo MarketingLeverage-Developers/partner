0df4129294573b81cd2f0a03b87e12360ecf048b
dhkim <dhkim@mleverage.co.kr>
2025-03-14 14:58:26 +0900

    feat: Menu 수정 - 고객지원 => 상담신청

--- src/headful/HeaderContent/Menu/Menu.tsx
+++ src/headful/HeaderContent/Menu/Menu.tsx
@@ -7,7 +7,7 @@ const Menu = () => {
             <MenuItem href="#license">회사소개</MenuItem>
             <MenuItem href="#merit">서비스 소개</MenuItem>
             <MenuItem href="#review">운송정보</MenuItem>
-            <MenuItem href="#contact">고객지원</MenuItem>
+            <MenuItem href="#contact">상담신청</MenuItem>
         </div>
     );
 };
